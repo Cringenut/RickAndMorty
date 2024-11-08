35d39af410df7e7ee07bdf1f72e998d77d0356fb
Cringenut <ihnatbaikou@gmail.com>
2024-11-08 12:27:49 +0100

    Finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 "use client"
 
+import { useRouter } from "next/navigation";
+import "../styles/CardPageStyle.css"
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-      <></>
+      <>
+          <div className="button-container">
+              <button onClick={() => router.push(`/character/${1}`)}>Characters</button>
+              <button onClick={() => router.push(`/character/${1}/episodes/${1}`)}>Episodes</button>
+              <button onClick={() => router.push(`/episode-list`)}>Episode List</button>
+          </div>
+      </>
   );
 }
